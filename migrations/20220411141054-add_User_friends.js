44fc318fa6e4182d2ca787c617ade253488d2e26
Elias Mouaheb <elias.mouaheb@etu.unistra.fr>
2022-04-11 18:07:28 +0200

    correction propre migrations et fk + ajout index

--- migrations/20220411141054-add_User_friends.js
+++ migrations/20220411141054-add_User_friends.js
@@ -2,29 +2,11 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.createTable('User_friends',{
-      id: {
-        type: Sequelize.INTEGER,
-        defaultValue: Sequelize.UUIDV4,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-      friend_id: {
-        type: Sequelize.UUID,
-        defaultValue: Sequelize.UUIDV4,
-        allowNull: false,
-      },
-      user_id: {
-        type: Sequelize.UUID,
-        defaultValue: Sequelize.UUIDV4,
-        allowNull: false,
-
-      }
-    });
+    
     await queryInterface.addConstraint('User_friends',{
       type: 'foreign key',
       name: 'friend_id_fk',
+      onDelete: 'CASCADE',
       fields: ['friend_id'],
       references: {
         table: 'Users',
@@ -34,12 +16,37 @@ module.exports = {
     await queryInterface.addConstraint('User_friends',{
       type: 'foreign key',
       name: 'user_id_fk',
+      onDelete: 'CASCADE',
       fields: ['user_id'],
       references: {
         table: 'Users',
         field: 'id'
       }
     });
+    await queryInterface.addConstraint('User_password_reset_token',{
+      type: 'foreign key',
+      name: 'user_id_fk',
+      onDelete: 'CASCADE',
+      fields: ['user_id'],
+      references: {
+        table: 'Users',
+        field: 'id'
+      }
+    }
+  );
+    await queryInterface.addIndex('User_friends', ['user_id', 'friend_id'], {
+      name: 'user_friends_user_id_friend_id_e1a66b48_uniq',
+      unique: true
+     });
+     await queryInterface.addIndex('User_friends', ['friend_id'], {
+      name: 'user_friends_friend_id_34769ea0'
+     });
+     await queryInterface.addIndex('User_friends', ['user_id'], {
+      name: 'user_friends_user_id_dd550e2d'
+     });
+     await queryInterface.addIndex('User_password_reset_token', ['user_id'], {
+      name: 'user_password_reset_tokens_user_id_e9b7220f'
+     });
     /**
      * Add altering commands here.
      *
@@ -50,11 +57,14 @@ module.exports = {
 
   async down(queryInterface, Sequelize) {
     await queryInterface.removeConstraint('User_password_reset_token', 'user_id_fk');
+    await queryInterface.removeConstraint('User_friends', 'user_id_fk');
     await queryInterface.removeConstraint('User_friends', 'friend_id_fk');
     await queryInterface.removeConstraint('User_friends', 'user_id_fk');
-    await queryInterface.dropTable('Users');
-    await queryInterface.dropTable('User_password_reset_token');
-    await queryInterface.dropTable('user_friends');
+    await queryInterface.removeIndex('User_friends', 'user_friends_user_id_friend_id_e1a66b48_uniq');
+    await queryInterface.removeIndex('User_friends', 'user_friends_friend_id_34769ea0');
+    await queryInterface.removeIndex('User_friends', 'user_friends_user_id_dd550e2d');
+    await queryInterface.removeIndex('User_password_reset_token', 'user_password_reset_tokens_user_id_e9b7220f');
+
       /**
      * Add reverting commands here.
      *
--- migrations/20220411151542-add_User_friends.js
+++ migrations/20220411151542-add_User_friends.js
@@ -1,6 +1,7 @@
 'use strict';
+
 module.exports = {
-  async up(queryInterface, Sequelize) {
+  async up (queryInterface, Sequelize) {
     await queryInterface.createTable('Users', {
       id: {
         type: Sequelize.UUID,
@@ -67,41 +68,23 @@ module.exports = {
         type: Sequelize.UUID
       }
     });
-    await queryInterface.addConstraint('User_password_reset_token',{
-        type: 'foreign key',
-        name: 'user_id_fk',
-        fields: ['user_id'],
-        references: {
-          table: 'Users',
-          field: 'id'
-        }
-      }
-    );
-    await queryInterface.addConstraint('User_friends',{
-      type: 'foreign key',
-      name: 'friend_id_fk',
-      fields: ['friend_id'],
-      references: {
-        table: 'Users',
-        field: 'id'
-      }
-    });
-    await queryInterface.addConstraint('User_friends',{
-      type: 'foreign key',
-      name: 'user_id_fk',
-      fields: ['user_id'],
-      references: {
-        table: 'Users',
-        field: 'id'
-      }
-    });
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
   },
+
   async down(queryInterface, Sequelize) {
-    await queryInterface.removeConstraint('User_password_reset_token', 'user_id_fk');
-    await queryInterface.removeConstraint('User_friends', 'friend_id_fk');
-    await queryInterface.removeConstraint('User_friends', 'user_id_fk');
     await queryInterface.dropTable('Users');
     await queryInterface.dropTable('User_password_reset_token');
-    await queryInterface.dropTable('user_friends');
+    await queryInterface.dropTable('User_friends');
+   
+   /*  * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
   }
 };
